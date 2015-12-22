4759e7573c7f0e73f5da2baf050646168a2cb1c8
Vilkov Kirill <wide.wrd@gmail.com>
2015-12-22 22:13:39 +0300

    remove old graphics

--- source/qaib/app/Application.cpp
+++ source/qaib/app/Application.cpp
@@ -1,9 +1,6 @@
 #include <qaib/app/Application.hpp>
 #include <SFML/Graphics.hpp>
 
-#include <qaib/scene/Sprite.hpp>
-#include <qaib/graphics/SceneRenderer.hpp>
-#include <qaib/scene/Camera.hpp>
 
 namespace qaib {
 
@@ -11,33 +8,12 @@ namespace qaib {
 	int Application::exec() {
 		window = new sf::RenderWindow(sf::VideoMode(800, 600), "Window");
 
-		sf::Texture texture;
-		texture.loadFromFile("../data/apple.jpg");
-		
-		SceneNode scene;
-
-		auto sprite = new Sprite(texture);
-		scene.addChild(sprite);
-
-		auto cam = new Camera();
-		scene.addChild(cam);
-
-		SceneRenderer renderer;
-
 		while (window->isOpen()) {
 			sf::Event event;
 			while (window->pollEvent(event)) {
 				if (event.type == sf::Event::Closed)
                     closeButtonClicked.fire();
 			}
-
-			window->clear(sf::Color(0, 0, 255, 255));
-
-			renderer.drawScene(window, &scene, cam);
-
-			window->display();
-
-			cam->setPosition(cam->getPosition() + sf::Vector2f(-0.01f, -0.01f));
 		}
 
 		return 0;
