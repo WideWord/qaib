9c5dba4b7c430011a89d74e765daa024b8708783
Kirill Vilkov <wide.wrd@gmail.com>
2016-10-12 22:51:06 +0300

    wip

--- source/qaib/nn/Population.cpp
+++ source/qaib/nn/Population.cpp
@@ -10,8 +10,8 @@ namespace qaib {
         Genome initialGenome(innovationGenerator, inputsCount, outputsCount);
         for (int i = 0; i < size; ++i) {
             auto genome = initialGenome;
-            for (int j = 0; j < 10; ++j) {
-                genome.mutate(innovationGenerator);
+            for (int j = 0; j < 4; ++j) {
+                genome.insertRandomConnection(innovationGenerator);
             }
             genomes.push_back(genome);
         }
